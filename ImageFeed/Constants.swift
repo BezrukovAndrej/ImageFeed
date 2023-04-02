75d2cbf2c3b7af408e03b31eb551447e102e74b4
Bezrukovandrej <bezrukovandrej@gmail.com>
2023-04-02 13:51:37 +0300

    Самостоятельная задача. Добавление: AccessKey, Secret key, Redirect URI, AccessScope, DefaultBaseURL

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let AccessKey = "JJ8eMw9mODPvSAhwi44jkw9QZoP2Jc-ZvyrcwnBtfHk"
+let SecretKey = "mKK1UATME8SQXLGFqDvPzX65azBUhFSx14qQ0sJuJgE"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
